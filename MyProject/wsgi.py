5a301cbed4ed0aa2f12aa0ff647b5267f5547303
school <school@MacBook-Pro.local>
2025-04-16 23:45:48 +0900

    Votre message de commit

--- MyProject/wsgi.py
+++ MyProject/wsgi.py
@@ -11,6 +11,7 @@ import os
 import django
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyProject.settings")
 
@@ -24,3 +25,11 @@ except Exception as e:
     print("Erreur lors de l'exécution des migrations :", e)
 
 application = get_wsgi_application()
+
+
+django.setup()
+User = get_user_model()
+
+# Créez un superuser si aucun n'existe (exemple de création avec des valeurs par défaut)
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('Lazare', 's2244145@gmail.com', 'IUGZ2752HGCS')
